97a027f16289a596262393f1663c107279c54bd7
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2023-07-20 14:45:07 +0300

    solution

---
+++ UnitTastingLab/src/test/java/rpg_lab/HeroTest.java
@@ -0,0 +1,24 @@
+package rpg_lab;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.Assert.*;
+
+public class HeroTest {
+
+    @Test
+    public void testHeroAttackWhenTargetDiesHeroGetsExp() {
+        int exp = 10;
+
+        Target facade = mock(Target.class);
+        when(facade.isDead()).thenReturn(true);
+        when(facade.giveExperience()).thenReturn(exp);
+        Weapon weapon = mock(Weapon.class);
+        Hero hero = new Hero("Pesho", weapon);
+        hero.attack(facade);
+        assertEquals(exp,hero.getExperience());
+    }
+
+}
